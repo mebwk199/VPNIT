6d539538ab788206206ef6c31ea479ac5fda3da0
mebwk199 <mebwk199@atomicmail.io>
2026-08-10 14:27:58 +0330

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('Content-Type: application/json; charset=utf-8');
-echo json_encode([
-  'status' => 'ok',
-  'service' => 'itVPN Telegram Bot',
-  'time' => date('c')
-], JSON_UNESCAPED_UNICODE);
